b161d3b8afe0f51c0ce3f174eb2edd3c01e9af68
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-05-05 17:14:46 -0300

    cree los servicios

---
+++ Service/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using CursoBlazorWebAssembly.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace CursoBlazorWebAssembly.Service
+{
+    public class CategoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly JsonSerializerOptions _serializerOptions;
+
+
+
+        public CategoryService(HttpClient httpClient, JsonSerializerOptions serializerOptions)
+        {
+            _httpClient = httpClient;
+            _serializerOptions = serializerOptions;
+        }
+
+
+
+        public async Task<List<Category>?> GetCategories() 
+        {
+            var response = await _httpClient.GetAsync("v1/categories");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+            return JsonSerializer.Deserialize<List<Category>>(content,_serializerOptions);
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            var response = await _httpClient.PostAsync("v1/categories", JsonContent.Create(category));
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var response = await _httpClient.DeleteAsync($"v1/categories/{categoryId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using CursoBlazorWebAssembly.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace CursoBlazorWebAssembly.Service
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly JsonSerializerOptions _serializerOptions;
+
+        public ProductService(HttpClient httpClient, JsonSerializerOptions options)
+        {
+            _httpClient = httpClient;
+            _serializerOptions = options;
+        }
+
+
+        public async Task<List<Product>?> GetProducts()
+        {
+            var response = await _httpClient.GetAsync("/v1/products");
+
+            return await JsonSerializer.DeserializeAsync<List<Product>>(await response.Content.ReadAsStreamAsync());
+        }
+
+
+        public async Task AddProducts(Product product)
+        {
+            var response = await _httpClient.PostAsync($"v1/products", JsonContent.Create(product));
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+
+        public async Task UpdateProduct(int productID, Product product)
+        {
+            var response = await _httpClient.PutAsync($"v1/products/{productID}", JsonContent.Create(product));
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+        public async Task DeleteProducts(int productID)
+        {
+            var response = await _httpClient.DeleteAsync($"v1/products/{productID}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+    }
+}
